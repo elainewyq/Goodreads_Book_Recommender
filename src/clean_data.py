570b421a2dcc610d3ad5d8a5be1b5bcc5fdb714f
Elaine Wu <elaine.wyq@gmail.com>
2019-11-06 01:25:34 -0800

    filter non english books and save cleaned data to a seprate csv

--- src/clean_data.py
+++ src/clean_data.py
@@ -29,13 +29,6 @@ def clean_reviews(reviews_df):
     start = pd.to_datetime('2000-01-01 00:00:00')
     df = reviews_df2[reviews_df2.timestamp>=start].copy()
 
-    # remap the user id and book ids so they start from 0.
-    print('remapping the ids')
-    df['old_user_id'] = df['user_id']
-    df['user_id'] = map_id(df['old_user_id'])
-
-    df['old_book_id'] = df['book_id']
-    df['book_id'] = map_id(df['old_book_id'])
     return df
 
 def clean_books(books_df):
@@ -56,29 +49,23 @@ def clean_books(books_df):
         # count = CountVectorizer()
         # count_matrix = count.fit_transform(books_df.genre)
         # genre_df = pd.DataFrame(count_matrix.todense(), index=books_df.book_id)
-    
-    books_df.language_code
-
-    books_df.drop(['work_id','isbn','asin', 'country_code', 'similar_books', 'link'], axis =1, inplace=True)
-    
-    ## revist the features below after creating a pipeline
-    books_df.drop(['description', 'author_id', 'language_code', 'publisher', 'publication_year'], axis =1, inplace=True)
-
-    #author_id is a dictionary with keys 'author_id' and 'role' and we only keep the first author
-    # books_df.author_id = books_df.author_id.apply(lambda x:x[0]['author_id'])
-     
-    #one encoder for 'is_ebook' and 'author_id'
-    #there are missing values in features 'languague_code' 'publisher' and 'publication_year' which are filled with 'empty str'. 
-    #we will keep them as they are and encode
-
-    #get enbedding for 'title', 'description' and 'genre'
         # genre_df.reset_index(inplace=True)
     books_df.drop(['genre'], axis =1, inplace=True)
         # books_df.reset_index(inplace=True)
         # df = pd.concat([books_df, genre_df],axis=1)
-    books_df.set_index(['book_id'], drop=True, inplace=True)
+    
 
-    return books_df
+    #author_id is a dictionary with keys 'author_id' and 'role' and we only keep the first author
+    books_df.author_id = books_df.author_id.apply(lambda x:x[0]['author_id'])
+
+
+    books_df.language_code = books_df.language_code.replace(r'^\s*$', np.nan, regex=True)
+    books_df1 = books_df[books_df.language_code.apply(lambda x:True if x in ['eng', 'en-GB', 'en-CA', 'en-US', 'en', np.nan] else False)].copy()
+    books_df1.drop(['work_id','isbn','asin', 'country_code', 'language_code', 'publication_year', 'description'], axis =1, inplace=True)  
+
+    books_df1.set_index(['book_id'], drop=True, inplace=True)
+
+    return books_df1
 
 def scale_data(df,label=False):
     sc = StandardScaler()
@@ -98,6 +85,16 @@ def scale_data(df,label=False):
     return final_df
 
 
+def review_map_id(df):
+    # remap the user id and book ids so they start from 0.
+    df['old_user_id'] = df['user_id']
+    df['user_id'] = map_id(df['old_user_id'])
+
+    df['old_book_id'] = df['book_id']
+    df['book_id'] = map_id(df['old_book_id'])
+
+    return df
+
 def map_id(s):
     unique_values = sorted(s.unique())
     mapping = {}
@@ -112,7 +109,7 @@ def genre_list(x):
     genre = ''
     try:
         for i in x:
-            if int(i['count']) >= 1:
+            if int(i['count']) >= 5:
                 genre += i['name'] +' '
         return genre
     except:
